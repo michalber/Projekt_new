ad443b394a315e144acccd354495024703e296e0
michalber <37382499+michalber@users.noreply.github.com>
2018-06-13 23:28:46 +0200

    Update1.1.0

--- Projekt_new/Projekt_new/Projekt_new.c
+++ Projekt_new/Projekt_new/Projekt_new.c
@@ -1,48 +1,36 @@
 /*
+ * Projekt programu do pomiaru ciœnienia wraz z obs³ug¹ przerwania
+   po którym wyœwietlana jest najwiêksza zmierzona wartoœæ
+ *
  * Created: 05.06.2018 09:56:59
- * Author : MB JB
- */ 
+ * Author : MB & JB
+*/
 //------------------------------------------------------------------
+//-- Biblioteki -- 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include "lcd.h"
 //------------------------------------------------------------------
-//-- Definicje LCD --
-#define F_CPU 1000000
-#define LCD_RS_PORT PORTC
-#define LCD_RW_PORT PORTC
-#define LCD_DATA0_PORT PORTC
-#define LCD_DATA1_PORT PORTC
-#define LCD_DATA2_PORT PORTC
-#define LCD_DATA3_PORT PORTC
-#define LCD_E_PORT PORTC
-
-#define LCD_DATA0_PIN 0
-#define LCD_DATA1_PIN 1
-#define LCD_DATA2_PIN 2
-#define LCD_DATA3_PIN 3
-
-#define LCD_RS_PIN 4
-#define LCD_E_PIN 5
-#define LCD_RW_PIN 6
-//-------------------
-
 //-- Definicje przetwornika ADC --
-#define ADCIN PB3  //definicja ADCIN (wejœcie analogowe PortB03)  
-//--------------------------------
-#define Vs 5
-#define LTHRES 500
-#define RTHRES 500
+#define ADCIN PORTB3  //definicja ADCIN (wejœcie analogowe PortB03)  
+#define F_CPU 1000000
+#define Vs 5.1
+//-------------------------------------------------------------------
+//-- Zmienne u¿ywane w programie -- 
+uint16_t adc_result0;
+uint16_t max_result;
+char int_buffer[10];
 //-------------------------------------------------------------------
 // Inicjalizacja przetwornika.
 void adc_init()
 {
 	//AREF = AVcc
-	ADMUX = (1<<REFS0);
-	
+	ADMUX = (1<<REFS1) |(1<<REFS0)|(1<<MUX2) | (1<<MUX0);	
 	// ADC w³¹czony,a prescaler ustawiony na 128
 	// 16000000/128 = 125000
 	ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+	//|(1<<MUX2) | (1<<MUX0)
 }
 //------------------------------------------------------------------
 // Czytanie wartoœci  przesy³anej na ADC
@@ -51,9 +39,9 @@ uint16_t adc_read(uint8_t ch)
 	// Wybieranie kana³u przetwornika
 	// Operacja AND z '7' zawsze da nam wartoœæ odcyztywan¹ przez przetwornik
 	// ch pomiêdzy 0 i 7
-	//ch &= 0b00000111;  // AND z 7
-	//ADMUX = (ADMUX)|ch;     // Wyczyszczenie trzech dolnych bitów przed operacj¹ OR
-	
+	ch &= 0b00000111;  // AND z 7
+	ADMUX = (ADMUX)|ch;    // Wyczyszczenie trzech dolnych bitów przed operacj¹ OR
+	//ADMUX = (ADMUX & 0xF8)|ch;
 	// Pocz¹tek pojedyñczej konwersji
 	// Zapisanie jedynki na ADSC
 	ADCSRA |= (1<<ADSC);
@@ -61,71 +49,59 @@ uint16_t adc_read(uint8_t ch)
 	//Czekanie a¿ konwersja siê zakoñczy
 	// ADC znowu zeruje siê
 	// Kontynuacja pêtli, a¿ do zakoñczenia
-	while(ADCSRA & (1<<ADSC));
-	
+	while(!(ADCSRA & (1<<ADIF)));
+	ADCSRA|=(1<<ADIF);
 	return (ADC);
 }
 
 //------------------------------------------------------------------
-int main()
+//-- Inicjalizacja przerwania -- 
+void break_init() {
+	 cli();    // na wszelki wypadek blokujemy przyjmowanie przerwañ
+	 MCUCR = (MCUCR & 0b1111100) | 0b10; // przerwanie przy zmianie INT0 1->0
+	 GIMSK |= (1<<INT0); // Uaktywniamy przerwanie INT0
+	 sei();   // odblokowujemy przyjmowanie przerwañ
+}
+// Obs³uga przerwania INT0 --
+ISR(INT0_vect)
 {
-	uint16_t adc_result0;
-	char int_buffer[10];
+	lcd_clear();
+	lcd_puts("Maks. wartosc");
+	itoa(max_result, int_buffer, 10);
+	lcd_set_cursor(0,1);
+	lcd_puts(int_buffer);
+	lcd_set_cursor(4,1);
+	lcd_puts("hPa");
 	
-	// initialize adc and lcd
+	_delay_ms(5000);
+}
+//------------------------------------------------------------------
+int main()
+{	
+	max_result = 0;
+	// inicjalizuj adc, lcd i przerwania
+	break_init(); 
 	adc_init();
-	lcd_init(LCD_DISP_ON_CURSOR);
-	lcd_clrscr();
-	lcd_home();
+	lcd_init();
+	lcd_on();
 	
-	// display the labels on LCD
-	lcd_puts("Odczyt = ");
 	_delay_ms(50);
 	
 	while(1)
 	{
+		lcd_clear();
+		lcd_puts("Odczyt = ");
 		adc_result0 = adc_read(0);      // odczytaj wartosc z ADC
 		
 		// wyœwietl na lcd
+		adc_result0=((adc_result0+0.085*Vs)/0.0412*Vs)/100;
+		if(adc_result0 > max_result) max_result=adc_result0;
 		itoa(adc_result0, int_buffer, 10);
-		adc_result0=(adc_result0+0.095*Vs)/0.009*Vs;
-		lcd_gotoxy(9,0);
+		lcd_set_cursor(9,0);
 		lcd_puts(int_buffer);
+		lcd_set_cursor(13,0);
+		lcd_puts("hPa");
 		
 		_delay_ms(1000);
 	}
 }
-//------------------------------------------------------------------
-/*
-
-int main(void)
-{
-	int wynik=0;
-	
-	ADCSRA = (1<<ADEN) //ADEN=1 w³¹czenie przetwornika ADC)
-	|(1<<ADPS0) // ustawienie preskalera na 128
-	|(1<<ADPS1)
-	|(1<<ADPS2)
-	
-	//ADMUX  =  (1<<REFS1) | (1<<REFS0) //REFS1:0: Reference Selection Bits
-	//Internal 2.56V Voltage Reference with external capacitor at AREF pin
-	//|(1<<MUX2) | (1<<MUX0); //Input Channel Selections (ADC5 - Pin 5 )
-	
-	DDRB &=~ (1<<ADCIN);        //Ustawienie Wejœcia ADC
-
-    while (1) 
-    {
-		ADCSRA |= (1<<ADSC); //ADSC: uruchomienie pojedynczej konwersji 
-		
-		 while(ADCSRA & (1<<ADSC)); //czeka na zakoñczenie konwersji  
-		 
-		 //konwertuj na ciœnienie: Vout=Vs*(0.009*P-0.095)
-		 // oblicz P: (Vout+0.095Vs)/0.009Vs, Vs=5;
-		 		 
-		 //wynik=konwersja;
-		 // wyswietl na LCD (biblioteka)
-		 //LCD <- wynik;
-    }
-}
-
-*/
